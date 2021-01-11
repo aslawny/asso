b522f20d137b808a7cf154c8242f48274913326c
aslawny <alexis.slawny@gmail.com>
2021-01-11 22:35:59 +0100

    homepage texte début homepage images

--- asso-front/src/views/Home.vue
+++ asso-front/src/views/Home.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <div class="square"></div>
+    <div class="square">
+      <div class="home">
+        "Ma conviction, appuyée sur 40 ans d'expérience, c'est que nul n'est inemployable,
+        sous reserve d'être aidé. Au moment où la croissance repart, (...) il est
+        essentiel de ne laisser personne au bord du chemin." <br />
+        Muriel Pericaud, Ministre du Travail
+        <div class="home-img">
+          <ul>
+            <li>
+              <router-link class="nav-link" to="/contact"
+                ><img src="../assets/reconversion.jpg"
+              /></router-link>
+            </li>
+            <li>
+              <router-link class="nav-link" to="/coaching"
+                ><img src="../assets/coaching.jpg"
+              /></router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
