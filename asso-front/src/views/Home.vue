b9b65bdd004769a27e39cb5a0f03945f520df60b
aslawny <alexis.slawny@gmail.com>
2021-01-12 22:24:25 +0100

    homepage carré orange

--- asso-front/src/views/Home.vue
+++ asso-front/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <div class="home">
+  <div class="home">
+    <div class="container">
       <br />"Ma conviction, appuyée sur 40 ans d'expérience, <br />c'est que nul n'est
       inemployable, sous reserve d'être aidé.<br />
       Au moment où la croissance repart, (...) <br />il est essentiel de ne laisser
       personne au bord du chemin." <br />
       Muriel Pericaud, Ministre du Travail<br /><br />
-      <div class="home-img">
+      <!--div class="home-img">
         <router-link to="/contact"><img src="../assets/reconversion.jpg" /></router-link>
-        &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+        &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
         <router-link to="/coaching"><img src="../assets/coaching.jpg" /></router-link>
-      </div>
+      </div-->
     </div>
   </div>
 </template>
-
-<style></style>
