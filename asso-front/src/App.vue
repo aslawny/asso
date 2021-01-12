9fb9277ad78a698173e407451567713090c45235
aslawny <alexis.slawny@gmail.com>
2021-01-12 21:43:42 +0100

    homepage images alignés horizontale

--- asso-front/src/App.vue
+++ asso-front/src/App.vue
@@ -46,7 +46,6 @@
       <br /><br /><br />
       <h4>La Shirley Family présente</h4>
       <h1>La Clinique RH</h1>
-      <img src="./assets/logo.png" />
       <h4>Depuis 2018</h4>
       <br /><br />
       <router-view />
--- asso-front/src/views/Home.vue
+++ asso-front/src/views/Home.vue
@@ -1,25 +1,15 @@
 <template>
   <div>
-    <div class="square">
-      <div class="home">
-        "Ma conviction, appuyée sur 40 ans d'expérience, c'est que nul n'est inemployable,
-        sous reserve d'être aidé. Au moment où la croissance repart, (...) il est
-        essentiel de ne laisser personne au bord du chemin." <br />
-        Muriel Pericaud, Ministre du Travail
-        <div class="home-img">
-          <ul>
-            <li>
-              <router-link class="nav-link" to="/contact"
-                ><img src="../assets/reconversion.jpg"
-              /></router-link>
-            </li>
-            <li>
-              <router-link class="nav-link" to="/coaching"
-                ><img src="../assets/coaching.jpg"
-              /></router-link>
-            </li>
-          </ul>
-        </div>
+    <div class="home">
+      <br />"Ma conviction, appuyée sur 40 ans d'expérience, <br />c'est que nul n'est
+      inemployable, sous reserve d'être aidé.<br />
+      Au moment où la croissance repart, (...) <br />il est essentiel de ne laisser
+      personne au bord du chemin." <br />
+      Muriel Pericaud, Ministre du Travail<br /><br />
+      <div class="home-img">
+        <router-link to="/contact"><img src="../assets/reconversion.jpg" /></router-link>
+        &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+        <router-link to="/coaching"><img src="../assets/coaching.jpg" /></router-link>
       </div>
     </div>
   </div>
